977516d3d23b98a47b2ca1a2e269f4233a9794f1
Antonio Faustino <faustinorocha1@gmail.com>
2026-01-31 12:26:29 -0300

    Removido os arquivos da API e Frontend do Bytebank

--- api-bytebank-v2-main/src/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-import { createConnection } from "./database.js";
-
-createConnection();
-app.listen(8000, () => {
-  console.log("Servidor rodando em http://localhost:8000");
-});
--- api-bytebank-v2-main/src/routes/saldo-routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getSaldo, updateSaldo } from "../controllers/saldo.controller.js";
-const router = Router();
-
-router.get("/users/:id/saldo", getSaldo);
-router.put("/users/:id/saldo", updateSaldo);
-
-export default router;
