8d096140be2ed8c5c026baeab96f59a68181b7da
Antonio Faustino <faustinorocha1@gmail.com>
2026-02-01 09:30:34 -0300

    Adicionar script npm para geração de relatórios do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
 });
